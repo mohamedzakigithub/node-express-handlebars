e19437274b45d87542a555f106b0fb7be856c6ae
Mohamed Eleraky <mohamedzaki@hotmail.com>
2020-05-07 16:37:30 +1000

    initial commit

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,59 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+  $(".change-devourStatus").on("click", function (event) {
+    var id = $(this).data("id");
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: true,
+    }).then(function () {
+      console.log("changed devour status to", true);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burger").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".delete-burger").on("click", function (event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted burger", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".reset").on("click", function (event) {
+    // Send the reset request.
+    $.ajax("/api/burgers/reset", {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted all burgers burger");
+      // Reload the page
+      location.reload();
+    });
+  });
+});
